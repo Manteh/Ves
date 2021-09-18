fc2dcfe1861263742ef8f57d05f326d41e7707ec
MantehApps <mantehapps@gmail.com>
2021-09-18 18:42:27 +0200

    Initial Commit

---
+++ Ves/VesApp.swift
@@ -0,0 +1,17 @@
+//
+//  VesApp.swift
+//  Ves
+//
+//  Created by Mantas Simanauskas on 2021-09-18.
+//
+
+import SwiftUI
+
+@main
+struct VesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
